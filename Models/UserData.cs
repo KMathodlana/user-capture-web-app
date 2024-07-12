446c35f030585a3c07f355374cce42beabdccdc3
Karabo <karabo@digitalmatter.com>
2024-07-12 13:12:23 +0200

    Add Views for Add, Edit, Form Fields and User Grid Add Model for UserData

---
+++ Models/UserData.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace UserCaptureTest1.Models
+{
+    [XmlType("User")]
+    public class UserData
+    {
+        [Key]
+        [XmlAttribute("UserId", DataType = "int")]
+        public int UserId { get; set; }
+
+        [XmlElement("FirstName")]
+        public required string FirstName { get; set; }
+
+        [XmlElement("LastName")]
+        public required string LastName { get; set; }
+
+        [XmlElement("Email")]
+        public required string Email { get; set; } //username and email can be used interchangebly 
+
+        [XmlElement("Cellphone")]
+        public required string Cellphone { get; set; }
+    }        
+}
---
+++ Pages/UserGrid.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UserCaptureTest1.Pages
+{
+    public class UserGridModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
